7678ca53b22f515a839dca9fa239713d14957fd2
Mobro <39844165+moyito2604@users.noreply.github.com>
2023-08-15 19:41:21 -0400

    Admin Check
    Ensures a User can use certain commands

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -25,25 +25,25 @@ class Admin(commands.Cog):
         if record:
             found = interaction.user.get_role(int(record['Role']))
         
-        if interaction.user.guild_permissions.administrator or found:
-            roletest = interaction.guild.get_role(int(role))
-            if not roletest:
-                await interaction.send("Invalid Role Provided, please try another role")
-                return
-            if record:
-                await interaction.send(f"Updating admin role to <@&{role}>")
-                cursor.execute(f"""UPDATE Admin_Roles SET
-                                Role = '{role}' WHERE
-                                Guild_id = '{interaction.guild.id}'""")
-            else:
-                await interaction.send(f"No record found. Creating an admin role record for <@&{role}>")
-                cursor.execute(f"""INSERT INTO Admin_Roles
-                                (Guild_id, Role) VALUES
-                                ('{interaction.guild.id}', '{role}')""")
-            settings.connection.commit()
-        else:
-            await interaction.send("You do not have the permissions to use this command")
+        if not interaction.user.guild_permissions.administrator and not found:
+            await interaction.send(f"You must have the <@&{record['Role']}> role to use this command")
             return
+        
+        roletest = interaction.guild.get_role(int(role))
+        if not roletest:
+            await interaction.send("Invalid Role Provided, please try another role")
+            return
+        if record:
+            await interaction.send(f"Updating admin role to <@&{role}>")
+            cursor.execute(f"""UPDATE Admin_Roles SET
+                            Role = '{role}' WHERE
+                            Guild_id = '{interaction.guild.id}'""")
+        else:
+            await interaction.send(f"No record found. Creating an admin role record for <@&{role}>")
+            cursor.execute(f"""INSERT INTO Admin_Roles
+                            (Guild_id, Role) VALUES
+                            ('{interaction.guild.id}', '{role}')""")
+        settings.connection.commit()
         
 def setup(client):
     client.add_cog(Admin(client))
